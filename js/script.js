64c1df9809855b8316eb2973ca672abaaa5e945a
gyangtto <gyangtto@gmail.com>
2024-02-14 23:17:42 +0900

    오류 수정 및 css 추가

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 let computerNum = 0; // 컴퓨터가 선택한 랜덤 숫자를 저장하는 변수
+let dartImg = document.querySelector('#dart-img'); // 다트 이미지 요소를 가져오는 변수
+let answerTxt = document.getElementById('answer_txt'); // 정답 텍스트를 출력하는 요소를 가져오는 변수
+let dartcoinImgs = document.querySelectorAll('.dart-coin'); // 다트 코인 이미지들을 가져오기
+let userNum = document.getElementById('user_number'); // 사용자가 입력한 숫자를 가져오는 변수
+
 let startArea = document.getElementById('start-area'); // 시작 영역 요소를 가져오는 변수
 let userName = document.getElementById('user_name'); // 사용자 이름 입력 요소를 가져오는 변수
-let userId = userName.value; // 사용자 아이디를 저장하는 변수
 let startBtn = document.getElementById('start_btn'); // 시작 버튼 요소를 가져오는 변수
 let submitArea = document.getElementById('submit-area'); // 제출 영역 요소를 가져오는 변수
 let submitBtn = document.getElementById('submit_btn'); // 제출 버튼 요소를 가져오는 변수
-let dartImg = document.querySelector('#dart-img'); // 다트 이미지 요소를 가져오는 변수
-let userNum = document.getElementById('user_number'); // 사용자가 입력한 숫자를 가져오는 변수
 
-let answerTxt = document.getElementById('answer_txt'); // 정답 텍스트를 출력하는 요소를 가져오는 변수
 let resetBtn = document.getElementById('reset_btn'); // 리셋 버튼 요소를 가져오는 변수
 let chances = 5; // 기회를 나타내는 변수
 let chancesTxt = document.getElementById('chance-txt'); // 기회 텍스트를 출력하는 요소를 가져오는 변수
@@ -68,7 +69,8 @@ const angleMapping = { // 숫자와 다트 회전 각도의 매핑
 };
 
 function playGame() {
-  let userValue = userNum.value; // 사용자가 입력한 숫자 가져오기
+  let userValue = parseInt(userNum.value, 10); // 사용자가 입력한 숫자를 숫자로 변환
+  console.log(userValue)
 
   if (userValue < 1 || userValue > 20) { // 입력된 숫자가 1부터 20 사이가 아닌 경우
     alert(`${userValue} 숫자는 1~20 범위를 벗어난 숫자입니다. 다른 숫자를 적어주세요.`); // 알림을 통해 메시지 출력
@@ -92,6 +94,7 @@ function playGame() {
     resultText = `정답! ${computerNum}`; // 결과 텍스트 설정
     alert(`${userName.value}님! 축하합니다! YOU WIN!`); // 알림을 통해 이겼음을 알림
     gameOver = true; // 게임 종료 상태로 변경
+    submitBtn.disabled = true; // 게임 종료 후에 제출 버튼 비활성화
   } else if (userValue > computerNum) { // 입력된 숫자가 정답보다 큰 경우
     resultText = 'Down!'; // 결과 텍스트 설정
   } else { // 입력된 숫자가 정답보다 작은 경우
@@ -115,7 +118,6 @@ function playGame() {
     chancesTxt.textContent = `남은 기회: ${chances}번`; // 남은 기회 텍스트 업데이트
 
     // 다트 펜 이미지 제어
-    const dartcoinImgs = document.querySelectorAll('.dart-coin'); // 다트 코인 이미지들을 가져오기
     for (let i = 0; i < dartcoinImgs.length; i++) { // 모든 다트 코인 이미지에 대해 반복
       if (i < chances) { // 현재 기회에 해당하는 다트 코인 이미지인 경우
         dartcoinImgs[i].style.display = 'block'; // 보이도록 설정
@@ -124,11 +126,11 @@ function playGame() {
       }
     }
 
-    if (chances === 0) { // 기회가 0이 된 경우
+    if (chances <= 0) { // 기회가 0 이하가 된 경우
       gameOver = true; // 게임 종료 상태로 변경
       userNum.disabled = true; // 숫자 입력란 비활성화
       submitBtn.disabled = true; // 제출 버튼 비활성화
-      alert(`${userName.value}님, 아쉽게도 모든 코인을 사용하셨습니다. Game Over!.`); // 알림을 통해 게임 종료 메시지 출력
+      alert(`${userName.value}님, 아쉽게도 모든 코인을 사용하셨습니다. Game Over!`); // 알림을 통해 게임 종료 메시지 출력
       const overAngle = angleMapping[computerNum]; // 정답에 해당하는 각도 가져오기
       dartImg.style.transform = `rotate(${overAngle}deg)`; // 다트 이미지를 해당 각도로 회전
     }
@@ -136,16 +138,28 @@ function playGame() {
 }
 
 function reset() {
+  gameOver = false; // 게임 종료 상태 해제
   userName.value = ''; // 사용자 이름 입력란 초기화
+  chances = 5; // 기회 초기화
+
+  history = []; // 기록 초기화
   userNum.value = ''; // 숫자 입력란 초기화
-  pickRandomNum(); // 정답 숫자 다시 선택
+  pickRandomNum(); // 정답 숫자 저장
+
+  userNum.disabled = false; // 숫자 입력란 활성화
+  submitArea.classList.remove('show'); // 제출 영역 숨김
+    submitBtn.disabled = false; // 제출 버튼 활성화
+
   dartImg.classList.remove('rotate'); // 다트 이미지 회전 효과 제거
   startArea.classList.remove('hidden'); // 시작 영역 보이도록 변경
-  submitArea.classList.remove('show'); // 제출 영역 숨김
-  resetBtn.style.display = 'none'; // 리셋 버튼 숨김
+
   chancesTxt.textContent = `남은 기회: 5 Coin`; // 남은 기회 텍스트 초기화
-  gameOver = false; // 게임 종료 상태 해제
-  history = []; // 기록 초기화
+    // 모든 다트 코인 이미지들을 한 번에 보이도록 처리
+    dartcoinImgs.forEach(function(coinImg) {
+      coinImg.style.display = 'block';
+    });
+
+  resetBtn.style.display = 'none'; // 리셋 버튼 숨김
 }
 
 pickRandomNum(); // 초기에 정답 숫자 선택
